5e65e9903203449b48d8c0c65bfd1d26384820ba
xiedl1996 <344919627@qq.com>
2017-04-21 19:51:45 +0800

    change name

--- app.js
+++ app.js
@@ -8,18 +8,46 @@ server.listen(process.env.PORT || 3000, function()
 {
    console.log('%s listening to %s', server.name, server.url); 
 });
-
-// Create chat bot
-var connector = new builder.ChatConnector
-({ appId: 'ac5c0529-a240-44e5-905c-502ad47e7bf2', appPassword: 'OszipQkatbA4VMTUskLD4Ke' }); 
-var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
-
-// Create bot dialogs
-bot.dialog('/', function (session) {
-    session.send("Hello World");
-});
 server.get('/', restify.serveStatic({
  directory: __dirname,
  default: '/index.html'
 }));
+// Create chat bot
+var connector = new builder.ChatConnector
+({ appId: 'ac5c0529-a240-44e5-905c-502ad47e7bf2', appPassword: 'OszipQkatbA4VMTUskLD4Ke' }); 
+var bot = new builder.UniversalBot(connector);
+var intents = new builder.IntentDialog();
+
+bot.dialog('/', intents);
+intents.matches(/^change name/i, [
+    function (session) {
+        session.beginDialog('/profile');
+    },
+    function (session, results) {
+        session.send('Ok... Changed your name to %s', session.userData.name);
+    }
+]);
+intents.onDefault([
+    function (session, args, next) {
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        session.send('Hello %s!', session.userData.name);
+    }
+]);
+
+// Create bot dialogs
+bot.dialog('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
