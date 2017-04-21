eb2493dbc091b0d5aacc0a138f4fbc91d43d8024
xiedl1996 <344919627@qq.com>
2017-04-21 16:37:55 +0800

    created package.json and app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+// Add your requirements
+var restify = require('restify'); 
+var builder = require('botbuilder'); 
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.PORT || 3000, function() 
+{
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat bot
+var connector = new builder.ChatConnector
+({ appId: 'YourAppId', appPassword: 'YourAppPassword' }); 
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+// Create bot dialogs
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
